2fc6668f5a195366fa717550a27f8c397f936340
harishv6665 <harishv6665@gmail.com>
2019-06-22 14:34:24 +0530

    css modules

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Home.css';
+import styles from './Home.css';
 
-const Home = () => <h1 className="title">Welcome to React!</h1>;
+const Home = () => <h1 className={styles.title}>Welcome to React!</h1>;
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,49 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/build',
-        publicPath: '/',
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: "./build",
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
       },
-    devtool: 'inline-source-map',
-    devServer: {
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js||jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader'],
+      {
+        test: /\.css$/i,
+        exclude: [/node_modules/, /global.css/],
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
             },
-            {
-                test: /\.(css)$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader'],
-            }
+          },
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ]
-}
+      },
+      {
+        test: [/global.css/],
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
